68b4b0e50e8cee3499f66e809de09018987ecf8e
kadepeay <kadepeay@gmail.com>
2020-05-07 00:11:52 -0600

    added timer

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import speedtest
+import time
 
+start = time.time()
 test = speedtest.Speedtest()
 download = test.download()
 upload = test.upload()
+end = time.time()
 
 print(f"Download Speed: {download} Upload Speed : {upload}")
+print("Done in " + str(end - start) + " seconds")
