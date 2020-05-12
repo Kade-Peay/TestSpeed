9dd6951b8bc40b485b80e818ddea0a7c8541afbe
kadepeay <kadepeay@gmail.com>
2020-05-12 15:00:04 -0600

    download and upload shown in mbps

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ start = time.time()
 test = speedtest.Speedtest()
 
 download = test.download()
-print(f"Download Speed: {download}", flush=True)
-
+print(f"Download Speed: {int(download / 1000000)} mbps", flush=True)
 
 upload = test.upload()
-print(f"Upload Speed: {upload}", flush=True)
+print(f"Upload Speed: {int(upload / 1000000)} mbps", flush=True)
 
 end = time.time()
 print("Done in " + str(end - start) + " seconds")
